f829e5ca2c88e166fb3da9dfb32c929d1b17e56c
Cao Khanh Nhat <knhat2014@gmail.com>
2021-09-22 23:26:24 +0700

    ^ init template

---
+++ template/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Navigator from './src/navigation';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Navigator />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ template/src/navigation/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const Navigator = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default Navigator;
